d064311d87683d3a0f9555b569c139e8e744456a
Emmanuel Béziat <contact@emmanuelbeziat.com>
2018-03-05 11:09:07 +0100

    cleanup scss + cleanup html + editorconfig + logo repo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,61 +1,80 @@
-function load_club(id)
-{
-	document.getElementById('club_nom').innerHTML = "Chargement...";
-	document.getElementById('club_img').innerHTML = "";
-	document.getElementById('club_presentation').innerHTML = "";
-	
-	$.getJSON("./api/ajax/get_club?id="+id).done(function(data){
+function load_club (id) {
+	document.getElementById('club_nom').innerHTML = 'Chargement…';
+	document.getElementById('club_img').innerHTML = '';
+	document.getElementById('club_presentation').innerHTML = '';
+
+	$.getJSON('./api/ajax/get_club?id=' + id).done(function (data) {
 		document.getElementById('club_nom').innerHTML = data.liste[0].nom;
-		document.getElementById('club_img').innerHTML = "<img src='"+data.liste[0].img+"' />";
+		document.getElementById('club_img').innerHTML = '<img src="' + data.liste[0].img + '">';
 		document.getElementById('club_presentation').innerHTML = data.liste[0].presentation;
 	});
 }
-function identification(from)
-{
+
+function identification (from) {
+	var date = new Date();
+
 	$('#error').hide();
 	$('#load').show();
-	var d = new Date();
-	if($('#stay').is(":checked"))var n = Math.trunc((d.getTime()+100*24*3600*1000)/1000); /* ConnectÃ© pour 100 jours */
-	else var n = Math.trunc((d.getTime()+4*3600*1000)/1000);
-	$.getJSON("./api/ajax/genere_token", {login:$('#mail').val(), pass:$('#passwd').val(), expiration: n }).done(function(data){
+
+	if ($('#stay').is(':checked')) {
+		// Connecté pour 100 jours
+		var n = Math.trunc((date.getTime() + 100 * 24 * 3600 * 1000) / 1000);
+	}
+	else {
+		var n = Math.trunc((date.getTime() + 4 * 3600 * 1000) / 1000);
+	}
+
+	$.getJSON('./api/ajax/genere_token', {
+		login: $('#mail').val(),
+		pass: $('#passwd').val(),
+		expiration: n
+	}).done(function (data) {
 		$('#load').hide();
-		if(data.error==1)
-		{
+
+		if (data.error === 1) {
 			$('#error_msg').html(data.msg);
 			$('#error').show();
 		}
-		else
-		{
+		else {
 			$('#ok').show();
 			$('#form').hide();
-			window.location="./espace_ZZ?token="+data.token+"&from="+from+"&expiration="+n;
-		}
-	});
-}
-function reset_pass()
-{
-	mail = $('#mail_pass_oublie').val();
-	html = $('#form_pass').html();
-	$('#form_pass').html("Envoi du mail en cours...");
-	$.getJSON("./api/ajax/change_passwd?mode=genere-token&mail="+mail).done(function(data){
-		if(data.error==0)$('#form_pass').html("Mail de récupération envoyé ! Pense a  vérifier les spams");
-		else 
-		{
-			alert("Impossible de trouver un compte !");
-			$('#form_pass').html(html);
+			window.location='./espace_ZZ?token=' + data.token + '&from=' + from + '&expiration=' + n;
 		}
 	});
 }
 
-function contact()
-{
-	nom = $('#name').val();
-	mail= $('#email').val();
-	provenance=$('#provenance').val();
-	msg=$('#message').val();
-	if(nom==""||mail==""||msg=="")return alert("Formulaire incomplet !");
-	$('#form_contact').html("Envoi du mail...");
-	$.post('./assets/php/contact.php', {nom:nom,mail:mail,provenance:provenance,msg:msg},function(data){
-		$('#form_contact').html("Mail envoyé ! Vous allez recevoir une copie par email et une réponse au plus vite.");
+function reset_pass () {
+	var $mail = $('#mail_pass_oublie').val();
+	var $html = $('#form_pass').html();
+
+	$('#form_pass').html('Envoi du mail en cours…');
+
+	$.getJSON("./api/ajax/change_passwd?mode=genere-token&mail=" + $mail).done(function(data){
+		if (data.error === 0)$('#form_pass').html("Mail de récupération envoyé ! Pense a  vérifier les spams");
+		else
+		{
+			alert('Impossible de trouver un compte !');
+			$('#form_pass').html($html);
+		}
+	});
+}
+
+function contact () {
+	var $nom = $('#name').val();
+	var $mail = $('#email').val();
+	var $provenance = $('#provenance').val();
+	var $msg = $('#message').val();
+
+	if ($nom === '' || $mail === '' || $msg === '') return alert('Formulaire incomplet !');
+
+	$('#form_contact').html('Envoi du mail…');
+
+	$.post('./assets/php/contact.php', {
+		nom: $nom,
+		mail: $mail,
+		provenance: $provenance,
+		msg: $msg
+	}, function (data) {
+		$('#form_contact').html('Mail envoyé ! Vous allez recevoir une copie par email et une réponse au plus vite.');
 	});
 }
--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 	ini_set("display_errors", 1);
 	require "./api/api.php";
+
+	$is_december = '12' === date('m') ? true : false;
 ?>
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <!--
 	SITE DU BDE DE L'ISIMA
 	- Crée par Louis MARTIN - BDE BliZZard
@@ -16,15 +18,19 @@
 <html>
 	<head>
 		<title>Site du BDE de l'ISIMA</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<meta name="msapplication-TileColor" content="#fee"/>
-        <meta name="theme-color" content="#fee">
-		<meta name="Description" content="Site du BDE de l'ISIMA, découvre la vie étudiante d'un ZZ (étudiant de l'ISIMA) sur notre site !" /> 
-		<meta name="Keywords" content="ISIMA, Clermont-Ferrand, Informatique, Etudiant, Vie etudiante, Clubs, Associations, <?php $clubs = api("get_liste_clubs");foreach($clubs['liste'] as $c)echo $c['nom'].", "; ?>Concours CCP, CPGE, Ingenieur, UBP, UCA, Universite Clermont-Auvergne, BREI, BNEI, plaquette alpha" />
-		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="msapplication-TileColor" content="#fee">
+		<meta name="theme-color" content="#fee">
+		<meta name="Description" content="Site du BDE de l'ISIMA, découvre la vie étudiante d'un ZZ (étudiant de l'ISIMA) sur notre site !">
+
+		<link rel="stylesheet" href="assets/css/main.css">
+		<?php if ($is_december) : ?>
+		<link rel="stylesheet" href="noel/neige.css">
+		<?php endif ?>
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css"><![endif]-->
+
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css"></noscript>
 		<link rel="apple-touch-icon" sizes="57x57" href="/ico/apple-icon-57x57.png">
 		<link rel="apple-touch-icon" sizes="60x60" href="/ico/apple-icon-60x60.png">
 		<link rel="apple-touch-icon" sizes="72x72" href="/ico/apple-icon-72x72.png">
@@ -43,109 +49,105 @@
 		<meta name="theme-color" content="#ffffff">
 	</head>
 	<body>
-		
 		<!-- Wrapper -->
-			<div id="wrapper">
-<?php
-			if(date("m") == '12')echo '
-				<link rel="stylesheet" href="noel/neige.css" />
-				<div class="snow" style="position:fixed!important">
-				  <div class="snow__layer"><div class="snow__fall"></div></div>
-				  <div class="snow__layer"><div class="snow__fall"></div></div>
-				  <div class="snow__layer">
+		<div class="wrapper">
+
+			<?php if ($is_december) : ?>
+			<div class="snow" style="position:fixed!important">
+				<div class="snow__layer"><div class="snow__fall"></div></div>
+				<div class="snow__layer"><div class="snow__fall"></div></div>
+				<div class="snow__layer">
 					<div class="snow__fall"></div>
 					<div class="snow__fall"></div>
 					<div class="snow__fall"></div>
-				  </div>
-				  <div class="snow__layer"><div class="snow__fall"></div></div>
 				</div>
-		
-			';
-		
-		 ?>
-				<!-- Header -->
-					<header id="header">
-						<div class="logo">
-							<img src="./images/logo.png" style="width:100%" />
-						</div>
-						<div class="content">
-							<div class="inner">
-								<h1>BDE ISIMA</h1>
-								<p><!--[-->Découvre la vie étudiante d'un ZZ (étudiant de l'ISIMA) dès maintenant ! <!--]--></p>
-							</div>
-						</div>
-						<nav>
-							<ul>
-								<li><a href="#actus">News</a></li>
-								<!--<li><a href="#bde">Le BDE</a></li>-->
-								<li><a href="#clubs">Clubs</a></li>
-								<li><a href="./espace_ZZ">Compte</a></li>
-                                                                <li><a href='https://drive.google.com/drive/folders/0B8UQ_-N6TCbvRDZEcUtTS1hWc2M?usp=sharing'>Annales</a></li>
-								<li><a href="#contact">Contact</a></li>
-							</ul>
-							<p><br /><a href="#actus"><em>DERNIERE NEWS: <?php echo api("get_news", array("nombre" => 1))['liste'][0]['titre']; ?></em></a></p>
-						</nav>
-					</header>
-
-				<!-- Main -->
-					<div id="main">
-
-							<article id="actus">
-								<?php include "pages/actus.php"; ?>
-							</article>
-
-							<article id="bde">
-								<?php include "pages/bde.php"; ?>
-							</article>
-
-							<article id="clubs">
-								<?php include "pages/clubs.php"; ?>
-							</article>
-
-							<article id="contact">
-								<?php include "pages/contact.php"; ?>
-							</article>
-
-							<article id="zz">
-								<?php include "pages/zz.php"; ?>
-							</article>
-							
-							<article id="partenaires">
-								<?php include "pages/partenaires.php"; ?>
-							</article>
-
-							<article id="details_club">
-								<?php include "pages/details_club.php"; ?>
-							</article>
-
-							<article id="oubli">
-								<?php include "pages/oubli.php"; ?>
-							</article>
-
-							<article id="create_account">
-								<?php include "pages/create_account.php"; ?>
-							</article>
-
-					</div>
-
-				<!-- Footer -->
-					<footer id="footer">
-						<p class="copyright">&copy; BDE ISIMA. Design: <a href="https://html5up.net">HTML5 UP</a>.
-						<br />Créé par BDE BliZZard. <a href="#partenaires">Nos partenaires</a></p>
-					</footer>
-
+				<div class="snow__layer"><div class="snow__fall"></div></div>
 			</div>
+			<?php endif; ?>
+
+			<!-- Header -->
+			<header class="header">
+				<div class="logo">
+					<img src="./images/logo.png" style="width:100%">
+				</div>
+
+				<div class="content">
+					<div class="inner">
+						<h1>BDE ISIMA</h1>
+						<p>Découvre la vie étudiante d’un ZZ (étudiant de l’ISIMA) dès maintenant !</p>
+					</div>
+				</div>
+
+				<nav class="menu">
+					<a href="#actus">News</a>
+					<!-- <a href="#bde">Le BDE</a> -->
+					<a class="menu__item" href="#clubs">Clubs</a>
+					<a class="menu__item" href="./espace_ZZ">Compte</a>
+					<a class="menu__item" href="https://drive.google.com/drive/folders/0B8UQ_-N6TCbvRDZEcUtTS1hWc2M?usp=sharing">Annales</a>
+					<a class="menu__item" href="#contact">Contact</a>
+					</ul>
+				</nav>
+				<a class="last-news" href="#actus"><span class="text-uppercase">Dernière news :</span> <?php echo api('get_news', array('nombre' => 1))['liste'][0]['titre']; ?></a>
+			</header>
+
+			<!-- Main -->
+			<main class="main">
+
+				<section class="section" id="actus">
+					<?php include "pages/actus.php"; ?>
+				</section>
+
+				<section class="section" id="bde">
+					<?php include "pages/bde.php"; ?>
+				</section>
+
+				<section class="section" id="clubs">
+					<?php include "pages/clubs.php"; ?>
+				</section>
+
+				<section class="section" id="contact">
+					<?php include "pages/contact.php"; ?>
+				</section>
+
+				<section class="section" id="zz">
+					<?php include "pages/zz.php"; ?>
+				</section>
+
+				<section class="section" id="partenaires">
+					<?php include "pages/partenaires.php"; ?>
+				</section>
+
+				<section class="section" id="details_club">
+					<?php include "pages/details_club.php"; ?>
+				</section>
+
+				<section class="section" id="oubli">
+					<?php include "pages/oubli.php"; ?>
+				</section>
+
+				<section class="section" id="create_account">
+					<?php include "pages/create_account.php"; ?>
+				</section>
+
+			</main>
+
+			<!-- Footer -->
+			<footer class="footer">
+				<p class="copyright">&copy; BDE ISIMA. Design: <a href="https://html5up.net">HTML5 UP</a>.
+				<br>Créé par BDE BliZZard. <a href="#partenaires">Nos partenaires</a></p>
+			</footer>
+
+		</div>
 
 		<!-- BG -->
-			<div id="bg"></div>
+		<div class="bg"></div>
 
 		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-			<script src="assets/js/scripts.js"></script>
-			<?php include ("./script_indispensable.php"); ?>
-
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/skel.min.js"></script>
+		<script src="assets/js/util.js"></script>
+		<script src="assets/js/main.js"></script>
+		<script src="assets/js/scripts.js"></script>
+		<?php include ('./script_indispensable.php'); ?>
 	</body>
 </html>
